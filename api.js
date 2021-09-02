7ddec10522eb16a22b03c4d21f5c9298db9f30c7
kongkonbiswas <kongkonbiswas3241@gmail.com>
2021-09-02 21:18:24 +0600

    corriction added

--- api.js
+++ api.js
@@ -1,5 +1,5 @@
 document.getElementById("error-message").style.display = "none";
-const searchFood = () => {
+const searchBook = () => {
   const searchField = document.getElementById("search-field");
   const searchText = searchField.value;
   // clear data
@@ -9,7 +9,7 @@ const searchFood = () => {
   const url = `https://openlibrary.org/search.json?q=${searchText}`;
   fetch(url)
     .then((res) => res.json())
-    .then((data) => displaySearchResult(data.docs))
+    .then((data) => displaySearchResult(data.docs, data.numFound))
     .catch((error) => displayError(error));
 };
 
@@ -17,7 +17,7 @@ const displayError = (error) => {
   document.getElementById("error-message").style.display = "block";
 };
 
-const displaySearchResult = (docs) => {
+const displaySearchResult = (docs, numFound) => {
   const searchResult = document.getElementById("search-result");
   searchResult.textContent = "";
   docs.forEach((doc) => {
@@ -25,6 +25,7 @@ const displaySearchResult = (docs) => {
     const div = document.createElement("div");
     div.classList.add("col");
     div.innerHTML = `
+    <h4>Result Found: ${numFound}</h4>
     <div onclick="loadMealDetail(${doc.cover_i})" class="card h-100">
         <img src="https://covers.openlibrary.org/b/id/${doc.cover_i}-M.jpg" class="card-img-top" alt="...">
         <div class="card-body">
@@ -35,6 +36,6 @@ const displaySearchResult = (docs) => {
     </div>
     `;
     searchResult.appendChild(div);
-  })
+  });
 };
 
